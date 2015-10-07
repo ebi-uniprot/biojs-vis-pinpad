69553b3e667d377eff744c3c580522b01f1c1929
ljgarcia <leylajael@gmail.com>
2015-10-07 13:16:43 +0100

    Create an element with sections and subsections.

--- snippets/simple.js
+++ snippets/simple.js
@@ -3,31 +3,107 @@ var app = require("biojs-vis-pinpad");
 var instance = new app({
     el: yourDiv
     , width: '250px'
+    , height: '300px'
     , toPin: {
         category: "Domains & sites",
         sections: [
             {
                 title: "Site 671-672",
                 information: {
-                    description: "Cleavage; by beta-secretase",
+                    string: "Cleavage; by beta-secretase",
                     number: 5,
-                    more_information: ["info1", "info2"],
-                    even_more: {
-                        title: "See also",
-                        other: "this information",
-                        more: "more info"
+                    boolean: true,
+                    link: {
+                        value: "any text",
+                        link: "https://wwwdev.ebi.ac.uk/uniprot/services/rest/uniprot/features/P05067"
                     },
-                    and_more: {
-                        anything: "See as well",
-                        what: ["here", "there"]
-                    },
-                    more_Info: [
+                    primitive_array: ["info1", "info2", 3, 2, false],
+                    mixed_array: [
                         {
-                            "name": "PubMed",
-                            "id": "12034808",
-                            "url": "http://www.ncbi.nlm.nih.gov/pubmed/12034808"
+                            value: "one",
+                            link: "https://wwwdev.ebi.ac.uk/uniprot/services/rest/uniprot/features/P05067"
+                        }, "two",
+                        {
+                            value: "three",
+                            link: "https://wwwdev.ebi.ac.uk/uniprot/services/rest/uniprot/features/P05067"
                         }
+                    ],
+                    styled_elem: {
+                        key: "Styled element",
+                        key_style: "text-align: right; background-color: green; color: blue;",
+                        value: "Primitive",
+                        value_style: "background-color: blue; color: green;"
+                    },
+                    styled_array: {
+                        key: "Styled array",
+                        key_style: "text-align: right; background-color: pink; color: purple;",
+                        value: ["elem1", 2, true],
+                        value_style: "background-color: purple; color: pink;"
+                    },
+                    styled_link: {
+                        key: "Styled link",
+                        value: {
+                            value: "click me!",
+                            link: "https://wwwdev.ebi.ac.uk/uniprot/services/rest/uniprot/features/P05067"
+                        }
+                    },
+                    styled_link_array: {
+                        key: "Styled mixed array",
+                        value: [
+                            {
+                                value: "click me once!",
+                                link: "https://wwwdev.ebi.ac.uk/uniprot/services/rest/uniprot/features/P05067"
+                            },
+                            {
+                                value: "click me twice!",
+                                link: "https://wwwdev.ebi.ac.uk/uniprot/services/rest/uniprot/features/P05067"
+                            },"do not click me!"
+                        ]
+                    },
+                    subsection_one: {
+                        title: "Subsection title",
+                        information: {
+                            primitive: "String",
+                            array: [1, 2, 3]
+                        }
+                    },
+                    complex_object: {
+                        key: "value",
+                        number_key: 3,
+                        array: [1, 2, 3],
+                        link: {
+                            value: "text",
+                            link: "https://wwwdev.ebi.ac.uk/uniprot/services/rest/uniprot/features/P05067"
+                        },
+                        links: [
+                            {
+                                value: "text2",
+                                link: "https://wwwdev.ebi.ac.uk/uniprot/services/rest/uniprot/features/P05067"
+                            },
+                            {
+                                value: "text3",
+                                link: "https://wwwdev.ebi.ac.uk/uniprot/services/rest/uniprot/features/P05067"
+                            }
+                        ],
+                        no_more: "better not to add more levels"
+                    },
+                    complex_array: [
+                        {question: "how does it look?"}, {answer: "a bit weird maybe?"}, "should we stop now?"
+                        , ["maybe", "we", "should"]
                     ]
+                }
+            },
+            {
+                title: "LSS information",
+                information: {
+                    frequency: 0.005,
+                    subsection_one: {
+                        title: "LSS subsection",
+                        information: {
+                            primitive: "String",
+                            array: [1, 2, 3]
+                        }
+                    }
                 }
             }
         ]
@@ -36,7 +112,42 @@ var instance = new app({
 });
 instance.addElement({
     category: "Domains & sites",
-    sections: []
+    sections: [
+        {
+            title: "Site 580",
+            information: {
+                frequency: 0.005,
+                subsection_one: {
+                    title: "Any subsection",
+                    information: {
+                        description: "Anything here"
+                    }
+                }
+            }
+        }
+    ]
+});
+instance.addElement({
+    category: "Domains & sites",
+    sections: [
+        {
+            title: "Site 342",
+            information: {
+                anything: 0.005
+            }
+        }
+    ]
+});
+instance.addElement({
+    category: "Domains & sites",
+    sections: [
+        {
+            title: "Site 425",
+            information: {
+                something: 0.005
+            }
+        }
+    ]
 });
 instance.addElement({
     category: "PTM",
